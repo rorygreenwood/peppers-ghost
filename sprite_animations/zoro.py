dd0794e198142b9d43b98445ba719532b661dfc3
rory <rorygreenwood@outlook.com>
2024-10-20 17:10:13 +0100

    initial commit

---
+++ sprite_animations/zoro.py
@@ -0,0 +1,53 @@
+idle_settings = {
+    "sprite_sheet_image": 'sprite_sheet_crops/zoro_nobg/zoro_stance.png',
+    "frame number": 4,
+    "initial step": 0,
+    "step jump": 1.03,
+    "frame height": 150,
+    "frame width": 120
+}
+
+walking_settings = {
+    "sprite_sheet_image": 'sprite_sheet_crops/zoro_nobg/zoro_left_new.png',
+    "frame number": 8,
+    "initial step": 0,
+    "step jump": 1.17,
+    "frame height": 160,
+    "frame width": 125
+}
+
+attack_settings = {
+    "sprite_sheet_image": 'sprite_sheet_crops/zoro_nobg/attack.png',
+    "frame number": 6,
+    "initial step": 0,
+    "step jump": 1.16,
+    "frame height": 190,
+    "frame width": 190
+}
+
+jump_settings = {
+    "sprite_sheet_image": 'sprite_sheet_crops/zoro_nobg/zoro_jump.png',
+    "frame number": 8,
+    "initial step": 0,
+    "step jump": 1,
+    "frame height": 190,
+    "frame width": 170
+}
+
+heavy_attack_settings = {
+    "sprite_sheet_image": 'sprite_sheet_crops/zoro_nobg/zoro_heavy_attack.png',
+    "frame number": 6,
+    "initial step": 0,
+    "step jump": 1.02,
+    "frame height": 180,
+    "frame width": 250
+}
+
+spin_settings = {
+    "sprite_sheet_image": 'sprite_sheet_crops/zoro_nobg/zoro_spin.png',
+    "frame number": 6,
+    "initial step": 0,
+    "step jump": 1,
+    "frame height": 185,
+    "frame width": 215
+}
